9596af99b2f476a413573266d0f1e034e21da810
francs1 <francs1@sina.com>
2019-07-11 09:13:51 +0800

    v0.3

---
+++ version_0.3/main.py
@@ -0,0 +1,41 @@
+from __future__ import division, print_function, unicode_literals
+
+#system module
+import sys
+import os
+import random
+
+#common mechine learning tools and frameworks
+import numpy as np
+import pandas as pd
+from pandas import Series,DataFrame
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import seaborn as sb
+import sklearn as skl
+
+#custom module
+from constantInit import *
+import modules
+
+import dataAnalysis as da
+import trainingModel as tm
+import preTreatment as pt
+
+
+def main():
+	#modules.checkVersion()
+	#da.analysis()
+
+	#pt.preProcessData()
+
+	#tm.trainSGD()
+	#tm.trainLR()
+	#tm.trainKNN()
+	#tm.trainDT()
+	tm.trainRF()
+	
+	
+if __name__ == '__main__':
+	main()
---
+++ version_0.3/trainingModel.py
@@ -0,0 +1,111 @@
+# trainingModel.py
+import sys
+
+import pandas as pd
+import numpy as np
+
+from sklearn.base import clone
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import Perceptron
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+
+
+from constantInit import *
+import dataLoad as dl
+
+import preTreatment as pt
+
+np.random.seed(42)
+
+
+def trainLR():
+	passenger_prepared,passenger_labels,test_set,passenger_test = pt.preProcessData()
+
+	lr_clf = LogisticRegression(C=1.0, penalty='l2', tol=1e-6, solver='lbfgs')
+	passenger_labels = passenger_labels.values.reshape((len(passenger_labels.values),))
+	lr_clf.fit(passenger_prepared, passenger_labels)
+
+	print(cross_val_score(lr_clf, passenger_prepared, passenger_labels, cv=3, scoring="accuracy"))
+
+	predict_data = lr_clf.predict(passenger_test)
+	PassengerIds = test_set['PassengerId']
+	results = pd.Series(predict_data,name="Survived",dtype=np.int32)
+	submission = pd.concat([PassengerIds,results],axis = 1)
+	dl.saveData(submission,'LRClassifier.csv')
+
+
+def trainSGD():
+	passenger_prepared,passenger_labels,test_set,passenger_test = pt.preProcessData()
+
+	sgd_clf = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42)
+	passenger_labels = passenger_labels.values.reshape((len(passenger_labels.values),))
+	sgd_clf.fit(passenger_prepared, passenger_labels)
+
+	print(cross_val_score(sgd_clf, passenger_prepared, passenger_labels, cv=3, scoring="accuracy"))
+
+	predict_data = sgd_clf.predict(passenger_test)
+	PassengerIds = test_set['PassengerId']
+	results = pd.Series(predict_data,name="Survived",dtype=np.int32)
+	submission = pd.concat([PassengerIds,results],axis = 1)
+
+	dl.saveData(submission,'SGDClassifier.csv')
+
+def trainDT():
+	passenger_prepared,passenger_labels,test_set,passenger_test = pt.preProcessData()
+
+	sgd_clf = DecisionTreeClassifier()
+	passenger_labels = passenger_labels.values.reshape((len(passenger_labels.values),))
+	sgd_clf.fit(passenger_prepared, passenger_labels)
+
+	print(cross_val_score(sgd_clf, passenger_prepared, passenger_labels, cv=3, scoring="accuracy"))
+
+	predict_data = sgd_clf.predict(passenger_test)
+	PassengerIds = test_set['PassengerId']
+	results = pd.Series(predict_data,name="Survived",dtype=np.int32)
+	submission = pd.concat([PassengerIds,results],axis = 1)
+
+	dl.saveData(submission,'DTClassifier.csv')
+
+def trainRF():
+	passenger_prepared,passenger_labels,test_set,passenger_test = pt.preProcessData()
+
+	sgd_clf = RandomForestClassifier(n_estimators=100)
+	passenger_labels = passenger_labels.values.reshape((len(passenger_labels.values),))
+	sgd_clf.fit(passenger_prepared, passenger_labels)
+
+	print(cross_val_score(sgd_clf, passenger_prepared, passenger_labels, cv=3, scoring="accuracy"))
+
+	predict_data = sgd_clf.predict(passenger_test)
+	PassengerIds = test_set['PassengerId']
+	results = pd.Series(predict_data,name="Survived",dtype=np.int32)
+	submission = pd.concat([PassengerIds,results],axis = 1)
+
+	dl.saveData(submission,'RFClassifier.csv')
+
+def trainKNN():
+	passenger_prepared,passenger_labels,test_set,passenger_test = pt.preProcessData()
+
+	sgd_clf = KNeighborsClassifier(n_neighbors = 3)
+	passenger_labels = passenger_labels.values.reshape((len(passenger_labels.values),))
+	sgd_clf.fit(passenger_prepared, passenger_labels)
+
+	print(cross_val_score(sgd_clf, passenger_prepared, passenger_labels, cv=3, scoring="accuracy"))
+
+	predict_data = sgd_clf.predict(passenger_test)
+	PassengerIds = test_set['PassengerId']
+	results = pd.Series(predict_data,name="Survived",dtype=np.int32)
+	submission = pd.concat([PassengerIds,results],axis = 1)
+
+	dl.saveData(submission,'KNNlassifier.csv')
+
